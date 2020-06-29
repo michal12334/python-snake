65d09f6d4a6b24b510c779ca739fee09c110e8a8
michal <michal1842001@gmail.com>
2020-06-29 18:23:46 +0200

    create a Rectangle class

---
+++ rectangle.py
@@ -0,0 +1,29 @@
+from vector2 import *
+import pygame
+
+class Rectangle:
+    def __init__(self, position = Vector2(0, 0), size = Vector2(0, 0)):
+        self.setPosition(position)
+        self.setSize(size)
+        self.color = 255, 255, 255
+
+    def setPosition(self, position):
+        self.position = position
+
+    def setSize(self, size):
+        self.size = size
+
+    def getPosition(self):
+        return self.position
+
+    def getSize(self):
+        return self.size
+
+    def setColor(self, r, g, b):
+        self.color = r, g, b
+
+    def getColor(self):
+        return self.color
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, (self.position.x, self.position.y, self.size.x, self.size.y))
