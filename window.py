11b343fd34894659bbfce8fe1ce4e19181754726
michal <michal1842001@gmail.com>
2020-06-30 10:02:27 +0200

    create Window class

---
+++ window.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Window:
+    def __init__(self, width = 300, height = 300):
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((self.width, self.height))
+
+    def draw(self, obj):
+        obj.draw(self)
+
+    def setFrameRateLimit(self, fps):
+        self.fps = fps
+
+    def setTitle(title):
+        pygame.display.set_caption(title)
