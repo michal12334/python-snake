ae36129b1a2e9be0c7ab22e7eff181c476e8fa33
michal <michal1842001@gmail.com>
2020-06-30 12:14:17 +0200

    create Circle class

---
+++ circle.py
@@ -0,0 +1,29 @@
+import pygame
+from vector2 import *
+
+class Circle:
+    def __init__(self, position = Vector2(0, 0), radious = 0):
+        self.setPosition(position)
+        self.setRadious(radious)
+        self.color = 255, 255, 255
+
+    def setPosition(self, position):
+        self.position = position
+
+    def getPosition(self):
+        return self.position
+
+    def setRadious(self, radious):
+        self.radious = radious
+
+    def getRadious(self):
+        return self.radious
+
+    def setColor(self, r, g, b):
+        self.color = r, g, b
+
+    def getColor(self):
+        return self.color
+
+    def draw(self, window):
+        pygame.draw.circle(window.getPygameSurface(), self.color, (self.position.x, self.position.y), self.radious)
