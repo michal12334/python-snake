e1a22d095eaafdf025fb65bf713846086d8e6176
michal <michal1842001@gmail.com>
2020-07-03 09:33:28 +0200

    factorize main.py

--- main.py
+++ main.py
@@ -3,28 +3,34 @@ from window import *
 from snake import *
 from apple import *
 
-window = Window(900, 900)
-window.setTitle("Snake")
-window.setFrameRateLimit(60)
-
-isOpen = True
-
-snake = Snake()
-
-apple = Apple()
-
-while isOpen:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            isOpen = False
-
+def gameLoop():
     snake.update()
 
     if snake.isAppleEaten(apple.getPosition()):
         apple.setPosition(snake.generateNewPositionForApple())
         snake.addNewPart()
 
-    window.clear()
     window.draw(snake)
     window.draw(apple)
-    window.display()
+
+def main():
+    window = Window(900, 900)
+    window.setTitle("Snake")
+    window.setFrameRateLimit(60)
+
+    isOpen = True
+
+    snake = Snake()
+
+    apple = Apple()
+
+    while isOpen:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isOpen = False
+
+        window.clear()
+        gameLoop()    
+        window.display()
+
+main()
