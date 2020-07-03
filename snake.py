4d4c11fcbc03a731cc965bc5872ed453db64c97c
michal <michal1842001@gmail.com>
2020-07-03 09:29:32 +0200

    add isDead method to Snake class

--- snake.py
+++ snake.py
@@ -105,3 +105,6 @@ class Snake:
         x = self.snakeParts[0].getPosition().x
         y = self.snakeParts[0].getPosition().y
         return x < 0 or x + self.SNAKE_PART_SIZE > 900 or y < 0 or y + self.SNAKE_PART_SIZE > 900
+
+    def isDead(self):
+        return self.isHitItself() or self.isHitWall()
